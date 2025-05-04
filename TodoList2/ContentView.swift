60d284b75e4ccda4e2c33eeadb9ab84626bc723f
Shohei <yukawa.shouhei@gmail.com>
2025-05-04 19:57:02 +0900

    Initial Commit

---
+++ TodoList2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoList2
+//
+//  Created by 湯川昇平 on 2025/05/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoList2/TodoList2App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList2App.swift
+//  TodoList2
+//
+//  Created by 湯川昇平 on 2025/05/04.
+//
+
+import SwiftUI
+
+@main
+struct TodoList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
