2df3b9d76961bb15b67f077b5e5c5b5cecf2aa23
Shohei <yukawa.shouhei@gmail.com>
2025-05-19 19:47:07 +0900

    説明ページの追加

---
+++ TodoList2/Views/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  TodoList2
+//
+//  Created by 湯川昇平 on 2025/05/18.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
